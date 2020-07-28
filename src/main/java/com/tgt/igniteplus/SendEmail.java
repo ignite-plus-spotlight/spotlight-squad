fc4c9c6bc7075a316f03a1b5919d85548f38fd81
Yashaswini99-tech <yashaswiniraju99@gmail.com>
2020-07-28 16:43:32 +0530

    included java util

--- src/main/java/com/tgt/igniteplus/SendEmail.java
+++ src/main/java/com/tgt/igniteplus/SendEmail.java
@@ -7,19 +7,28 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Scanner;
+
+
 
 public class SendEmail {
 
     public static void main(String[] args) {
-
-        final String username = "btsignite99@gmail.com";
-        final String password = "yashu2020";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the username");
+        final String username = sc.nextLine();
+        System.out.println("enter the password");
+        final String password = sc.nextLine();
+        System.out.println("enter the fromEmail");
+        String fromEmail=sc.nextLine();
+        System.out.println("enter the toEmail");
+        String toEmail=sc.nextLine();
 
         Properties prop = new Properties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        prop.put("mail.smtp.starttls.enable", "true");
 
         Session session = Session.getInstance(prop,
                 new javax.mail.Authenticator() {
@@ -31,12 +40,11 @@ public class SendEmail {
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("btsignite@gmail.com"));
-            message.setRecipients(
-                    Message.RecipientType.TO,
-                    InternetAddress.parse("yashaswiniraju99@gmail.com")
-            );
+            message.setFrom(new InternetAddress(fromEmail));
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress(toEmail));
+
             message.setSubject("Received Award");
+
             Multipart emailContent = new MimeMultipart();
 
             //textbodypart
@@ -55,7 +63,7 @@ public class SendEmail {
             message.setContent(emailContent);
 
             Transport.send(message);
-            System.out.println("Message sent");
+            System.out.println("Message sent successfully");
 
         } catch (MessagingException e) {
             e.printStackTrace();
