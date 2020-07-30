afdb76e11849ee58b49b7424cc5db7ec10dd8cab
Yashaswini99-tech <yashaswiniraju99@gmail.com>
2020-07-30 20:43:18 +0530

    k

--- src/main/java/com/notify/App.java
+++ src/main/java/com/notify/App.java
@@ -1,6 +1,9 @@
 package com.notify;
 
+import com.notify.util.SendEmail;
 import com.notify.util.VelToPDF;
+
+import java.io.File;
 
 public class App {
     public static void main(String[] args) {
@@ -9,6 +12,7 @@ public class App {
             CertGenerator certGenerator = new CertGenerator();
             String htmlData = certGenerator.certGenerate();
             VelToPDF.velocityToPdf(htmlData);
+           // SendEmail.sendmail("btsignit99@gmail.com","yashu2020","btsignite99@gmail.com","yashaswiniraju99@gmail.com", new File("Test.pdf"));
 
         }catch (Exception e){
             e.printStackTrace();
