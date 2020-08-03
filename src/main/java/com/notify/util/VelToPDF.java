010e7c23ab41014de486251209dd789cd09c2dac
Yashaswini99-tech <yashaswiniraju99@gmail.com>
2020-08-03 12:07:53 +0530

    clean up ignored files

---
+++ src/main/java/com/notify/util/VelToPDF.java
@@ -0,0 +1,48 @@
+package com.notify.util;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+
+public class VelToPDF {
+
+    public static void velocityToPdf( String html){
+
+        PdfWriter pdfWriter = null;
+        Document document = new Document();
+        try {
+
+            //Rectangle rect = new Rectangle(700,500);
+            Rectangle rect = new Rectangle(700,400);
+            document.setPageSize(rect);
+
+            OutputStream file = new FileOutputStream(new File("Test.pdf"));
+
+
+            pdfWriter = PdfWriter.getInstance(document, file);
+
+            // open document
+            document.open();
+
+            XMLWorkerHelper xmlWorkerHelper = XMLWorkerHelper.getInstance();
+            xmlWorkerHelper.getDefaultCssResolver(true);
+            xmlWorkerHelper.parseXHtml(pdfWriter, document, new StringReader(html));
+
+
+            System.out.println("----------------------####PDF generated successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // close the document
+            document.close();
+            // close the writer
+            pdfWriter.close();
+        }
+    }
+}
